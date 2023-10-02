c1451fa5d105d873e34e90da76592f1b95b310db
matheusbs8 <matheusbs@dcc.ufrj.br>
2023-10-01 22:44:16 -0300

    InsertionSort - versao funcional - troca de intrucoes - bltz faltante

--- Mips/InsertionSort.asm
+++ Mips/InsertionSort.asm
@@ -1,6 +1,7 @@
 .data
 A: .word 3, 2, 7, 8, 1, 9, 10, 4, 7  # Array A com espaço para 9 inteiros
 tamanho: .word 9            # Variável tamanho para armazenar o tamanho do array
+A_addr: .word A  # Declara o endereço de A
 
 .text
 .globl main
@@ -10,14 +11,14 @@ main:
     lw $t0, tamanho
 
     # Inicializando o índice i com 1
-    li $t1, 1
+    addi $t1, $t1, 1
     
 for_i_loop:
     # Se i >= tamanho, sair do loop
     bge $t1, $t0, end
 
     # Carregando o endereço de A[i] (eleito)
-    la $t5, A
+    lw $t5, A_addr
     sll $t7, $t1, 2   # Multiplicando $t1 por 4 (tamanho de um elemento)
     add $t5, $t5, $t7 # $t5 contém o endereço de A[i]
 
@@ -29,21 +30,22 @@ for_i_loop:
     
 while_loop:
     # Se j < 0 ou A[j] <= eleito, sair do loop
+    # verificar um equivalente
     bltz $t3, end_while
 
     # Carregando o endereço de A[j]
-    la $t6, A
+    lw $t6, A_addr
     sll $t8, $t3, 2   # Multiplicando $t3 por 4 (tamanho de um elemento)
     add $t6, $t6, $t8 # $t6 contém o endereço de A[j]
 
     # Carregando A[j] na variável $t4
-    lw $t4, ($t6)
+    lw $t4, 0($t6)
 
     # Comparando eleito com A[j]
     bge $t2, $t4, end_while
 
     # A[j+1] = A[j]
-    sw $t4, ($t5)
+    sw $t4, 0($t5)
 
     # Decrementando j
     sub $t3, $t3, 1
@@ -56,7 +58,7 @@ while_loop:
 
 end_while:
     # A[j+1] = eleito
-    sw $t2, ($t5)
+    sw $t2, 0($t5)
 
     # Incrementando i
     addi $t1, $t1, 1
